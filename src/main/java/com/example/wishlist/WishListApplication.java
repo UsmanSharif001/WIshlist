37d56c78a5f935ebe8d871f3b17a38ee17a60d1f
UsmanSharif0001 <ussh0001@kea.stud.dk>
2024-04-08 15:10:09 +0200

    Start project WishList

---
+++ src/main/java/com/example/wishlist/WishListApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wishlist/WishListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
