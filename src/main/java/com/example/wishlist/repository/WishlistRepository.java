455b9572b7fdb0e71721469de084da1b2bf70784
MuLangballe <mala0007@stud.kea.dk>
2024-04-09 14:17:12 +0200

    Comments in WishlistRepository

--- src/main/java/com/example/wishlist/repository/WishlistRepository.java
+++ src/main/java/com/example/wishlist/repository/WishlistRepository.java
@@ -14,5 +14,22 @@ public class WishlistRepository {
     @Value("${spring.datasource.password}")
     private String pwd;
 
+    //Metode der henter liste af users fra databasen
+
+    //Metode der opretter og gemmer en user i databasen
+
+    //Metode der henter wishlist på userID
+
+    //Metode der opretter og gemmer en wishlist i databasen
+
+    //Metode der sletter en liste på UserID
+
+    //Metode der getWishes??? (burde den hedde get wish?)
+
+    //Metode der opretter og gemmer et ønske i databasen
+
+    //Metode der update/edit og gemmer et ønske i databasen
+
+    //Metode der sletter ønske på wishID
 
 }
